509553fb9b8f20a1731483d40d7c51e5d0fedf9c
Matteo <teoc98@github.com>
2023-03-16 23:11:26 +0100

    updated readme for bot3.py

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ Just a Telegram bot that sends daily notifications about weather forecast for pe
 
 ### Usage
 Install dependecies with `pip3 install requirements.txt`. 
-Run `python3 bot.py` in an environment where the following variables are defined:
+Run `python3 bot3.py` or `python3 bot.py` in an environment where the following variables are defined:
 - `OPEN_WEATHER_API_KEY` (see https://openweathermap.org/api) 
 - `TELEGRAM_TOKEN` (see https://core.telegram.org/bots/api)
 - `TELEGRAM_CHATS_ID`: a comma separated list of Telegram chat IDs
+
+Note:
+- `bot3.py` uses (One Call API 3.0)[https://openweathermap.org/api/one-call-3] with "One Call by Call" subscription plan (recommended option)
+- `bot.py` uses the Professional API collections with different fixed-price subscriptions (see (princing)[https://openweathermap.org/price]); a free plan is available  
 
--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from openweather import OpenWeatherAPI
 from requests import Request, Session
 from requests_cache import CachedSession
 import numpy as np
@@ -14,49 +15,13 @@ _pretty_printer = _pprint.PrettyPrinter(sort_dicts=False)
 pprint = _pretty_printer.pprint
 
 
-## OpenWeather API wrapper
-class OpenWeatherAPI:
-    base_url = "https://api.openweathermap.org"
-
-    def __init__(self, key):
-        self.key = key
-
-    ## returns a requests.Request; usage: response = session.send(request.prepare())
-    def request(self, url, **params):
-        params["appid"] = self.key
-        return Request("GET", self.base_url + "/" + url, params=params)
-
-    ## Direct geocoding: converts the specified name of a location or
-    ## zip/post code into the exact geographical coordinates
-    ## (https://openweathermap.org/api/geocoding-api)
-    def direct_geocoding(self, city_name, country_code, state_code=None, limit=None):
-        if state_code is not None:
-            query = [city_name, state_code, country_code]
-        else:
-            query = [city_name, country_code]
-
-        params = {"q": ",".join(query)}
-        if limit is not None:
-            params["limit"] = limit
-
-        return self.request("geo/1.0/direct", **params)
-
-    ## Call 5 day / 3 hour forecast data (https://openweathermap.org/forecast5)
-    def forecast1(self, lat, lon, **params):
-        return self.request("data/2.5/forecast", lat=lat, lon=lon, **params)
-
-    ## Call 16 day / daily forecast data, non-Free (https://openweathermap.org/forecast16)
-    def forecast_daily(self, **params):
-        return self.request("data/2.5/forecast/daily", **params)
-
-
 ## constructs a message about a city's weather
 def city_message(city):
     r = owSession.send(
         owAPI.forecast1(city["lat"], city["lon"], units="metric").prepare()
     ).json()
 
-    today_data = r["list"][:8]
+    today_data = r["list"][:8]  # uses the next 8 forecasts, covering the next 24 hours
     temp_min = min(float(x["main"]["temp_min"]) for x in today_data)
     temp_max = max(float(x["main"]["temp_max"]) for x in today_data)
 
@@ -78,6 +43,7 @@ with open("openweather-icons-info.json") as f:
 
 for icon, data in weather_icon_infos.items():
     data["value"] = np.array(data["value"])
+
 
 def get_weather_info(weather_icon):
     return weather_icon_infos[weather_icon[:2]]
@@ -102,17 +68,19 @@ def old_average(values, e=1):
 
     return avg
 
+
 def average(values, e=1):
     values = np.array(values)
     powers = np.float_power(abs(values), e) * np.sign(values)
     mean_powers = np.mean(powers, axis=0)
-    return np.float_power(abs(mean_powers), 1/e) * np.sign(mean_powers)
+    return np.float_power(abs(mean_powers), 1 / e) * np.sign(mean_powers)
 
 
 def distance(x, y, e=2):
     # r = sum((a - b) ** e for a, b in zip(x, y))
     r = np.float_power(abs(x - y), e).sum()
     return r
+
 
 def nearest_icon(avg_value):
     return min(
